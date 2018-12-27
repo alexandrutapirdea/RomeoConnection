fb529ec6bf05fe3124002bcab3579615dec1106c
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2018-12-27 22:17:45 +0200

    first commit

---
+++ RomeoConnection/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RomeoConnection.Models;
+
+namespace RomeoConnection.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            var user = GetUsers();
+
+            return View(user);
+        }
+        private IEnumerable<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User { firstName ="John", lastName = "Giumanca" },
+                new User { firstName ="Alex", lastName = "Cojocaru" },                
+            };
+        }
+    }
+}
