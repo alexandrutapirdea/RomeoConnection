44c4a4822d666eb2cba52d2153c436e13a1a4909
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2018-12-30 00:06:20 +0200

    Implemented API - to be tested

---
+++ RomeoConnection/Controllers/GroupMembersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using RomeoConnection.Models;
+using System.Web.Http;
+
+namespace RomeoConnection.Controllers
+{
+    [Authorize]
+    public class GroupMembersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public GroupMembersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult GroupMember([FromBody]int groupId)
+        {
+            var member = new GroupMember()
+            {
+                GroupId = groupId,
+                GroupMemberUserId = User.Identity.GetUserId(),
+            };
+            _context.GroupMembers.Add(member);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
