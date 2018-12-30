dfa021fd0d91c12322433bf5e85fbfc8a8df8240
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2018-12-30 19:32:02 +0200

    Added MyFriends list

--- RomeoConnection/Controllers/UsersController.cs
+++ RomeoConnection/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
-﻿using RomeoConnection.Models;
+﻿using Microsoft.AspNet.Identity;
+using RomeoConnection.Models;
 using RomeoConnection.ViewModels;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace RomeoConnection.Controllers
@@ -26,6 +28,25 @@ namespace RomeoConnection.Controllers
 
             return View(usersViewModel);
         }
+
+        [Authorize]
+        public ActionResult MyFriends()
+        {
+            var userId = User.Identity.GetUserId();
+            var friends = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+            //Daca primesti null pointer exception trebuie sa adaugi Include(ceva => ceva.altceva),
+            // unde ceva si altceva sunt proprietatile din alte tabele ( de exemplu pentru calendar)
+            var viewModel = new UserViewModel()
+            {
+                Users = ApplicationUsersToUsersList(friends),
+                ShowActions = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+
+        }
         private IEnumerable<User> GetUsers()
         {
             var listOfApplicationUsers = _context.Users;
@@ -48,5 +69,28 @@ namespace RomeoConnection.Controllers
             }
             return listOfUsers;
         }
+
+        private IEnumerable<User> ApplicationUsersToUsersList(List<ApplicationUser> listOfApplicationUsers)
+        {
+            var listOfUsers = new List<User>();
+
+            foreach (var user in listOfApplicationUsers)
+            {
+                var temporaryUser = new User();
+                temporaryUser.Description = user.Description;
+                temporaryUser.Birthday = user.BirthDay;
+                temporaryUser.FirstName = user.FirstName;
+                temporaryUser.LastName = user.LastName;
+                temporaryUser.JobTitle = user.JobTitle;
+                temporaryUser.Location = user.Location;
+                temporaryUser.DisplayId = user.Id;
+
+
+                listOfUsers.Add(temporaryUser);
+
+            }
+            return listOfUsers;
+        }
+
     }
 }
