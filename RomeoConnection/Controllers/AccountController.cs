469626eba00a9d751ccb8e82609327d5f84c65f9
= <ALEXANDRU.TAPIRDEA@my.fmi.unibuc.ro>
2019-01-03 18:07:00 +0200

    Added roles2

--- RomeoConnection/Controllers/AccountController.cs
+++ RomeoConnection/Controllers/AccountController.cs
@@ -172,6 +172,7 @@ namespace RomeoConnection.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    UserManager.AddToRole(user.Id, "RegularUser");
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
